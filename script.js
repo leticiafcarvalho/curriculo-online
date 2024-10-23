1466db8eca2b4a521b538d2849a92f7fa0aa6cc3
leticiafcarvalho <leticiacarvalhos2@outlook.com>
2024-10-23 11:10:33 -0300

    melhoria cv

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+document.querySelectorAll('.experience-item h3').forEach(item => {
+    item.addEventListener('click', event => {
+        const parent = item.parentElement;
+        const ul = parent.querySelector('ul');
+        ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+    });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
-    const sections = document.querySelectorAll('section');
-    sections.forEach((section) => {
-        section.style.opacity = 0;
-        setTimeout(() => {
-            section.style.opacity = 1;
-            section.style.transition = "opacity 0.5s";
-        }, 500);
+    const experienceItems = document.querySelectorAll('.experience-item');
+
+    experienceItems.forEach(item => {
+        const description = item.querySelector('ul');
+        description.style.display = 'none'; // Esconde a descrição inicialmente
+
+        item.addEventListener('click', () => {
+            // Alterna a visibilidade da descrição
+            if (description.style.display === 'none') {
+                description.style.display = 'block';
+                item.style.cursor = 'default';
+            } else {
+                description.style.display = 'none';
+                item.style.cursor = 'pointer';
+            }
+        });
     });
 });
